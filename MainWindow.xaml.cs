a6e003b158a469c8f01275758ede0b5486718390
Vorobevev <Vorobevev@202D-11>
2025-09-13 16:16:35 +0700

    update zd_1 130925

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,9 +21,12 @@ namespace laba_2_060925
             InitializeComponent();
         }
 
+        private int currentColorIndex = 0;
+
         private void MenuItem_Click_close(object sender, RoutedEventArgs e)
         {
             Close();
+
         }
 
         private void MenuItem_Click_white_bg(object sender, RoutedEventArgs e)
@@ -47,6 +50,108 @@ namespace laba_2_060925
         private void MenuItem_Click_developers(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Разботчик - Light");
+        }
+
+        private SolidColorBrush[] _colorList =
+        {
+            Brushes.Red,
+            Brushes.Green,
+            Brushes.White
+        };
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Background = _colorList[currentColorIndex];
+
+            currentColorIndex = (currentColorIndex + 1) % _colorList.Length;
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разботчик - Light");
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_MouseEnter_white_bg(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Белый цвет";
+        }
+
+        private void MenuItem_MouseEnter_red_bg(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Красный цвет";
+        }
+
+        private void MenuItem_MouseEnter_green_bg(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Зеленый цвет";
+        }
+
+        private void MenuItem_MouseEnter_developers(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Информация о разработчике";
+        }
+
+        private void MenuItem_MouseEnter_close(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Закрыть программу";
+        }
+
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Поменять цвет фона";
+        }
+
+        private void Button_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Информация о разработчике";
+        }
+
+        private void Button_MouseEnter_3(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "Закрыть программу";
+        }
+
+        private void MenuItem_MouseLeave_white_bg(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
+        }
+
+        private void MenuItem_MouseLeave_red_bg(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
+        }
+
+        private void MenuItem_MouseLeave_green_bg(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
+        }
+
+        private void MenuItem_MouseLeave_developers(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
+        }
+
+        private void MenuItem_MouseLeave_close(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
+        }
+
+        private void Button_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
+        }
+
+        private void Button_MouseLeave_2(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
+        }
+
+        private void Button_MouseLeave_3(object sender, MouseEventArgs e)
+        {
+            StatusText.Text = "";
         }
     }
 }
